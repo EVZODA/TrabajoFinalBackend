f2fc1fa1f3da079fa48a9159d723a65372519000
EVZODA <enzogiacoia@hotmail.com>
2022-11-29 17:21:02 -0300

    finished work

--- src/services/users/addUserService.js
+++ src/services/users/addUserService.js
@@ -2,6 +2,8 @@ const BreakingModel = require ('../../models/usuarios.models')
 
 const addUserService = async (req) => {
     const user = req.body;
+    const userBody = await BreakingModel.findOne({name: user.name})
+    if (userBody) throw new Error ("El usuario ya existe")
     const newUser = new BreakingModel(user);
     await newUser.save()
     return user;
--- src/services/users/updateUserService.js
+++ src/services/users/updateUserService.js
@@ -4,6 +4,8 @@ const updateUserService = async (req) => {
     const {name} = req.params
     const user = req.body
     const updateUser = await BreakingModel.findOne({name})
+    const userBody = await BreakingModel.findOne({name: user.name})
+    if (userBody) throw new Error ("El usuario no puede ser editado con un mismo nombre")
     updateUser.name = user.name
     updateUser.lastName = user.lastName
     updateUser.ocupattion = user.ocupattion
